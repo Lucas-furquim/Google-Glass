53c26439b5fbb677332ce14a3851e08cf41f848f
Lucas Furquim <lucasvinicius8970@gmail.com>
2022-02-15 17:46:54 -0300

    site terminado

--- aula 001/projeto-glass-html5/_javascript/funcoes.js
+++ aula 001/projeto-glass-html5/_javascript/funcoes.js
@@ -1,3 +1,9 @@
 function mudaFoto (foto){
     document.getElementById('icone').src = foto;
 }
+
+function calc_total(){
+    var qtd = parseInt(document.getElementById('quantidade').value);
+    tot = qtd * 1500;
+    document.getElementById('preco').value = tot;
+}
